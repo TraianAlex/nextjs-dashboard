64de72527a24bbcf08016dd62ff45d0d87220c0d
Traian Alexandru <victor_traian@yahoo.com>
2026-04-02 23:31:42 -0400

    search and pagination

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -7,15 +7,17 @@ import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
 import { Suspense } from 'react';
 import { fetchInvoicesPages } from '@/app/lib/data';
 
-export default async function Page(props: {
+export default async function Page({
+  searchParams,
+}: {
   searchParams?: Promise<{
     query?: string;
     page?: string;
   }>;
 }) {
-  const searchParams = await props.searchParams;
-  const query = searchParams?.query || '';
-  const currentPage = Number(searchParams?.page) || 1;
+  const searchParamsResolved = await searchParams;
+  const query = searchParamsResolved?.query || '';
+  const currentPage = Number(searchParamsResolved?.page) || 1;
   const totalPages = await fetchInvoicesPages(query);
 
   return (
